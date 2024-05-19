0691c02eeaaa7f81d78ea20618034d8c4200ea4d
profsam97 <proftoby4real@hotmail.com>
2024-05-19 17:09:15 +0100

    added pages  const

---
+++ src/constants/pages.ts
@@ -0,0 +1,7 @@
+import { PageI } from "../interfaces/page.interface";
+import { RouteNames } from "./routeNames";
+
+export const pages : PageI[] = [{
+    pageName: 'favorite',
+    pageLink : `${RouteNames.FAVOURITE}`
+}];
