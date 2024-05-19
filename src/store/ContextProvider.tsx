a7bc1b32a547c93fc628927ac61825f55aaf51fe
profsam97 <proftoby4real@hotmail.com>
2024-05-19 16:56:56 +0100

    added a wrapper for our context api

---
+++ src/store/ContextProvider.tsx
@@ -0,0 +1,68 @@
+import React, {useCallback, useState} from "react";
+import ContextApi from "./ContextApi";
+import { MealI } from "../interfaces/meal.interface";
+interface IProvider {
+    children: React.ReactNode
+}
+const ContextProvider  : React.FC<IProvider>= ({children}) => {
+    //this component makes our state global, that is accessible component-wide
+    const [category, setCategory] = useState<string>('')
+    const  [hasClicked, setHasClicked] = useState<boolean>(false);
+    const [searchTerm, setSearchTerm] = useState<string>('')
+    const [favourites, setFavourites] = useState<MealI[]>([])
+    const [mode, setMode] = useState<boolean>(false)
+
+    const handleSwitchMode = useCallback(() => {
+        console.log('switching')
+        setMode((prev) => !prev)
+    }, [mode])
+    const updateSearchTerm = (term : string) => {
+        setSearchTerm(term)
+    }
+
+    const handleAddFavourite = useCallback((meal : MealI) => {
+        console.log('..adding')
+        const newFavourite = favourites;
+
+        newFavourite.push(meal);
+
+        setFavourites(newFavourite)
+        console.log(favourites)
+    },[favourites])
+
+    const handleRemoveFavourite = useCallback((mealId : string) => {
+
+        setFavourites((prevMeal) => {
+            return prevMeal.filter((meal : MealI) => meal.idMeal !== mealId);
+        })
+      
+    },[])
+    const handleUpdateCategory = (category : string) => {
+        setCategory(category)
+    }
+    const handleHasClicked = () => {
+        setHasClicked(true)
+    }
+
+        const content = {
+                category,
+                handleUpdateCategory,
+                hasClicked,
+                handleHasClicked,
+                searchTerm,
+                updateSearchTerm,
+                handleAddFavourite,
+                handleRemoveFavourite,
+                favourites,
+                mode,
+                handleSwitchMode
+        }
+        return (
+            <ContextApi.Provider value={content}>
+                {children}
+            </ContextApi.Provider>
+    )
+
+}
+
+export default ContextProvider
