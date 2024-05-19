fdfcf2b531f2022d5967b4d148eb1017f9f864cd
profsam97 <proftoby4real@hotmail.com>
2024-05-19 17:56:10 +0100

    deleted redundant file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import {RouteNames} from "../constants";
 import Home from "../pages/home";
-import Category from "../pages/category";
 import MealDetails from "../pages/meal/mealdetails";
 import Favourite from "../pages/favourite";
 
@@ -10,7 +9,6 @@ export const AppRoutes = () => {
         <Routes>
             <Route path={RouteNames.HOME} element={<Home/>} />
 
-            <Route path={RouteNames.CATEGORY} element={<Category/>} />
 
             <Route path={RouteNames.VIEW_MEAL} element={<MealDetails/>}/>
 
