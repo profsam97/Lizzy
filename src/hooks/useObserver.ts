760288253519e3a014f52beb582bb5597ca50530
profsam97 <proftoby4real@hotmail.com>
2024-05-19 17:04:50 +0100

    added custom hook to manage infinite scrolling

---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const useObserver =  (isFetchingNextPage : boolean, hasNextPage : boolean | undefined, fetchNextPage: () => {}) => {
+
+    const observer = React.useRef<IntersectionObserver>();
+    const lastMealElementRef = React.useCallback(
+      (node : any) => {
+        if (isFetchingNextPage) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting && hasNextPage) {
+            fetchNextPage();
+          }
+        });
+        if (node) observer.current.observe(node);
+      },
+      [isFetchingNextPage, fetchNextPage, hasNextPage])
+
+      return lastMealElementRef
+}
