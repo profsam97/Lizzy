cca31bdbb32b19331e71f82caad62bcc05080042
profsam97 <proftoby4real@hotmail.com>
2024-05-19 17:14:37 +0100

    added Favourites components

---
+++ src/Components/Favourites.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MealI } from "../interfaces/meal.interface";
+import ContextApi from "../store/ContextApi";
+import Holder from "./Wrappers/Holder";
+import {  Grid, Stack, Typography } from "@mui/material";
+import MealCard from "../utils/MealCard";
+
+const Favourites : React.FC = () => {
+    const favourites : MealI[] = React.useContext(ContextApi).favourites;
+
+    return (
+        <Holder>
+            <Stack  direction={'column'} minWidth={'100%'}>
+                {favourites.length === 0 && <Typography variant="h6" textAlign={'center'} >No Favourite added yet</Typography>}
+            <Grid container spacing={2}>
+                {favourites.map((meal, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4}>
+                        <MealCard meal={meal} />
+                    </Grid>
+                ))}
+            </Grid>
+            </Stack>
+
+        </Holder>
+    )
+}
+
+export default Favourites;
