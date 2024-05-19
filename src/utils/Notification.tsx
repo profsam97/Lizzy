eda08c60debb4ab8d740f65e114e9769517673ed
profsam97 <proftoby4real@hotmail.com>
2024-05-19 16:53:32 +0100

    Added Notification util

---
+++ src/utils/Notification.tsx
@@ -0,0 +1,22 @@
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const Notification : React.FC = () => {
+
+    return (
+        <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+/>
+    )
+}
+
+export default Notification;
