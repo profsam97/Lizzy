28a38b855a73f236535fb784976d1142023a55dd
profsam97 <proftoby4real@hotmail.com>
2024-05-19 17:00:26 +0100

    added our favourite  page

---
+++ src/pages/favourite/index.tsx
@@ -0,0 +1,11 @@
+import Favourites from "../../Components/Favourites";
+
+
+const Favourite : React.FC = () => {
+
+        return (
+                <Favourites />
+        )
+}
+
+export default Favourite;
